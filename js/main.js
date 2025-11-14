3f36a508ec5fc15602c47fd0ff19659bb57c65ad
4NDR3S07 <andressrv07@gmail.com>
2025-11-13 22:08:54 -0500

    Tercer Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+<script>
+  function iniciarSesion() {
+      window.location.href = "../html/login.html"
+  };
+
+  function registrarse() {
+      window.location.href = "../html/registro.html"
+  };
+</script>
+        
+        
